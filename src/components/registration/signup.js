089968227510dd3c02099da481d39f6190607604
Seling Chen <seling.chen@yahoo.com>
2017-02-24 16:42:12 -0800

    bug fixes

--- src/components/registration/signup.js
+++ src/components/registration/signup.js
@@ -4,6 +4,12 @@ export default class extends Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.handleSignUp = this.handleSignUp.bind(this);
+    }
+
+    handleSignUp(e){
+        e.preventDefault();
+        
     }
 
     render() {
@@ -12,19 +18,21 @@ export default class extends Component {
             	<div className="signUpForm col-md-4">
                 	<h1>Sign Up</h1>
                 	<hr />
-                	<form>
+                	<form onSubmit={this.handleSignUp}>
   						<div className="form-group">
-    						<input type="email" placeholder="Email Address" id="email" className="form-control" /><br />
+    						<input type="email" placeholder="Email Address" id="email" ref="email" className="form-control" /><br />
     						<input type="password" placeholder="Password" id="passowrd" className="form-control" /><br />
     						<input type="text" placeholder="Username" id="username" className="form-control" /><br />
     						<table>
-    							<tr>
-    								<td id="spaceTd"><input type="text" placeholder="First name" id="first_name" className="form-control" /><br /></td>
-    								<td><input type="text" placeholder="Last name" if="last_name" className="form-control" /><br /></td>
-    							</tr>
+                                <tbody>
+        							<tr>
+        								<td id="spaceTd"><input type="text" placeholder="First name" id="first_name" className="form-control" /><br /></td>
+        								<td><input type="text" placeholder="Last name" id="last_name" className="form-control" /><br /></td>
+        							</tr>
+                                </tbody>
     						</table>
 
-                    	<label for="birthDate">Date of Birth</label>
+                    	<label id="birthDate">Date of Birth</label>
                         <input type="date" id="birthDate" className="form-control" /><br />
 
     					<button className="btn btn-primary">Create Account</button>
