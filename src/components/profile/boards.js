b487a70b0ff81a534cdf2fe72e5206e1992f005a
stacywong0402 <ninjastacyla@gmail.com>
2017-03-14 18:20:52 -0700

    pin UI

--- src/components/profile/boards.js
+++ src/components/profile/boards.js
@@ -107,7 +107,7 @@ class Boards extends Component {
                     <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12" key={index}> 
                         <Link to={"/board/" + board.id}>
                             <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 panel panel-danger boards">
-                                <div className="panel-body">
+                                <div className="panel-body boardheight">
                                    Image
                                 </div>
 
