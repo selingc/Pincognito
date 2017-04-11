b5d8761fd12a845da54e2bbc02626dda1cdf50ac
Stacy Wong <stacy@Stacys-MacBook-Pro.local>
2017-04-11 13:13:58 -0700

    changed ui for edit pin

--- src/components/popup/editPin.js
+++ src/components/popup/editPin.js
@@ -27,6 +27,15 @@ class EditPin extends Component {
         }
     }
 
+    deletePin(){
+        var confirmation = confirm("Are you sure you want to remove this pin?");
+        if (confirmation) {
+            this.props.deleteBoardPin(this.props.user.username, this.props.pin.boardID, this.props.pin.pinID, this.props.pin.createdBy);
+            this.closePopup();
+        }
+    }
+
+
     render() {
         return (
             <div>
@@ -38,17 +47,11 @@ class EditPin extends Component {
                     null
                 )}
                 <form className="createForm" onSubmit={this.editPin.bind(this)}>
-                    <input type="file" accept="image/*" className="form-control-file" id="image" ref="image"/> <br />
-                    <select className="form-control" ref="board" id="dropdown" defaultValue={this.props.pin.boardID}>
-                        <option value="none" disabled>--Select a Board--</option>
-                        {this.props.userBoards.map((board, index) => (
-                            <option value={board.boardID} key={index}>{board.name}</option>
-                        ))}
-                    </select><br />
                     <input type="text" className="form-control" ref="name" placeholder="Pin name" defaultValue={this.props.pin.name}/> <br />
                     <input type="text" className="form-control" ref="description" placeholder="Description" defaultValue={this.props.pin.description}/> <br />
                     <input type="text" className="form-control" ref="tags" placeholder="Tags separated by commas (ex. dog, cat, ...)" defaultValue={this.props.pin.tags}/> <br />
-                    <center><button type="submit" className="btn btn-danger">Confirm Changes</button></center>
+                    <button className="btn btn-danger" onClick={this.deletePin.bind(this)}>Delete</button>
+                    <button type="submit" className="btn btn-default confirmChangeButton">Confirm Changes</button>
                 </form>
             </div>
         );
