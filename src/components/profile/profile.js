b746cb74b059407a2118b31371067a0228dd89de
Sydney Snyder <syd.sny@gmail.com>
2017-04-28 15:09:18 -0700

    added tabs

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -4,16 +4,34 @@ import Boards from "./boards.js";
 import Pins from "./pins.js";
 
 class Profile extends Component {
+    constructor(props){
+        super(props);
+        this.state = {boardsorpins:"boards"};
+        this.changeTab = this.changeTab.bind(this);
+
+    }
+    changeTab(boardsorpins){
+        this.setState({boardsorpins: boardsorpins});
+    }
     render() {
         return (
             <div className="children">
                 <h1>@{this.props.user.username}</h1>
-                <h3>Your Boards</h3>
-                <hr />
-                <Boards/>
-                <h3>Your Pins</h3>
-                <hr />
-                <Pins/>
+                <div className="container">
+                <ul className="nav nav-tabs">
+                    <li className={this.state.boardsorpins==="boards"?"active":""}><a href="#boards" onClick={this.changeTab.bind(null, "boards")} data-toggle="tab">Your Boards</a></li>
+                    <li className={this.state.boardsorpins==="pins"?"active":""}><a href="#pins" onClick={this.changeTab.bind(null, "pins")} data-toggle="tab">Your Pins</a></li>
+                </ul>
+                </div>
+                <br />
+                <div className="tab-content">
+                    <div id="boards" className={this.state.boardsorpins==="boards"?"active tab-pane":"tab-pane"}>
+                        <Boards/>
+                    </div>
+                    <div id="pins" className={this.state.boardsorpins==="pins"?"active tab-pane":"tab-pane"}>
+                        <Pins/>
+                    </div>
+                </div>
             </div>
         );
     }
